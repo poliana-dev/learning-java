4f0264b22d00edecdc8469ed3603355c7ce47bca
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu>
2025-08-15 20:06:21 -0300

    refactor: correção no if

--- userInput.java
+++ userInput.java
@@ -13,14 +13,14 @@ class UserInput{
         int userAge = idade.nextInt();
         System.out.println("Você tem " + userAge + " anos");
 
-        if (userAge<=18){
-            System.out.println("Você é maior de idade");
+        if (userAge<18){
+            System.out.println("Você é menor de idade");
         }
         else if (userAge>=18 && userAge<=29){
             System.out.println("Você tem direito a carteira de estudante");
         }
         else{
-            System.out.println("Você não é maior de idade");
+            System.out.println("Você é maior de idade");
         }
 
         String age = (userAge>29) ? "Você é maior que 29 anos" : "Você deve procurar um estágio";
