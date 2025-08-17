6ee02076471a8757f6d4128139c5387aa4e351b5
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2025-08-17 17:15:33 -0300

    feat: primeiros passos em poo Tentando combinar com a classe Scanner para ter interação com o código.

---
+++ poo/alunos/Main.java
@@ -0,0 +1,20 @@
+package poo.alunos;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner dados = new Scanner(System.in);
+        
+        Aluno alunoUm = new Aluno();
+        System.out.print("Digite o nome do aluno: ");
+        alunoUm.nome_aluno = dados.nextLine();
+
+        System.out.print("Digite o idade do aluno: ");
+        alunoUm.idade_aluno = dados.nextInt();
+
+        System.out.print("Digite a nota do aluno: ");
+        alunoUm.nota_aluno= dados.nextInt();
+
+        alunoUm.exibirInfo();
+        dados.close();
+    }
+}
