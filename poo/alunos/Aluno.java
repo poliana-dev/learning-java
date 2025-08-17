2a95abe8d382c0c6fdf78fb9401b91d14018a5a7
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu>
2025-08-17 17:15:33 -0300

    feat: primeiros passos em poo Tentando combinar com a classe Scanner para ter interação com o código.

---
+++ poo/alunos/Aluno.java
@@ -0,0 +1,15 @@
+package poo.alunos;
+
+public class Aluno {
+    // atributos 
+    String nome_aluno;
+    int idade_aluno;
+    double nota_aluno;
+
+    // metodos
+    public void exibirInfo(){
+        System.out.println("Nome do aluno: " + nome_aluno + " Idade: " + idade_aluno + " Nota: " + nota_aluno);
+    }
+}
+
+
