b2f59e63e950de6d57fe804f00156abcaeb96a58
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2025-08-17 17:13:13 -0300

    feat: soma um número menos ele mesmo Aprendendo recursão

---
+++ methods/Soma.java
@@ -0,0 +1,19 @@
+package methods;
+
+public class Soma {
+  static int soma(int number) {
+
+    /* se number maior que zero; some number com number - 1, até 'zerar' */
+    if (number > 0) {
+      return number + soma(number - 1);
+    } else {
+      return 0;
+    }
+  }
+
+  public static void main(String[] args) {
+    int resulto = soma(2);
+    System.out.println(resulto);
+  }
+  
+}
